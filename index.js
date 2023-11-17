9dde245ed815b7208de21af17b57aecbe00102af
Pedro Henrique <pedrinhoavare18@gmail.com>
2023-11-17 10:11:26 -0300

    min 20:46 do vídeo

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    console.log(requisicao.body)
+})
+
+app.get('/cadastrar', (requisicao,resposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
 app.get('/usuarios/:id', (requisicao, resposta) => {
     const id = requisicao.params.id
     console.log(`acessando dados do usuário ${id}`)
